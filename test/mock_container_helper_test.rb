7a95940dbd5b2006f127fb37a27e52300c4101a5
Jim Weirich <jim.weirich@gmail.com>
2013-05-07 19:29:36 -0400

    Add tests for METHOD_NAME_RE

---
+++ test/mock_container_helper_test.rb
@@ -0,0 +1,68 @@
+require 'test/test_setup'
+
+class MockContainerHelperTest < Test::Unit::TestCase
+  include FlexMock::TestCase
+
+  def assert_method_name(name)
+    assert_match(FlexMock::MockContainerHelper::METHOD_NAME_RE, name)
+  end
+
+  def assert_not_method_name(name)
+    refute_match(FlexMock::MockContainerHelper::METHOD_NAME_RE, name)
+  end
+
+  def test_valid_method_names
+    assert_method_name "foo"
+    assert_method_name "Foo"
+    assert_method_name "_foo"
+    assert_method_name "__foo"
+    assert_method_name "___foo"
+    assert_method_name "_"
+    assert_method_name "foo_bar"
+    assert_method_name "foo__bar"
+    assert_method_name "foo_bar_"
+    assert_method_name "foo12"
+    assert_method_name "foo_bar_12"
+    assert_method_name "foo?"
+    assert_method_name "foo!"
+    assert_method_name "foo="
+    assert_method_name "+"
+    assert_method_name "-"
+    assert_method_name "*"
+    assert_method_name "/"
+    assert_method_name "&"
+    assert_method_name "|"
+    assert_method_name "^"
+    assert_method_name "~"
+    assert_method_name "=~"
+    assert_method_name "!~"
+    assert_method_name "`"
+    assert_method_name "!"
+    assert_method_name "**"
+    assert_method_name "+@"
+    assert_method_name "-@"
+    assert_method_name "=="
+    assert_method_name "!="
+    assert_method_name "==="
+    assert_method_name "<="
+    assert_method_name ">="
+    assert_method_name "<"
+    assert_method_name ">"
+    assert_method_name "<=>"
+    assert_method_name "[]"
+    assert_method_name "[]="
+  end
+
+  def test_invalid_method_names
+    assert_not_method_name ""
+    assert_not_method_name "1"
+    assert_not_method_name "1foo"
+    assert_not_method_name "foo!!"
+    assert_not_method_name "foo!?"
+    assert_not_method_name "foo?="
+    assert_not_method_name "foo@"
+    assert_not_method_name "++"
+    assert_not_method_name "!!"
+    assert_not_method_name "~="
+  end
+end
