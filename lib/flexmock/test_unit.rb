642e5fd3397c94fc9ad265121af61e0ad74b797f
Jim Weirich <jim.weirich@gmail.com>
2013-05-07 15:47:05 +0000

    Extracted TestCase extensions into their own files.

--- lib/flexmock/test_unit.rb
+++ lib/flexmock/test_unit.rb
@@ -12,42 +12,7 @@
 require 'flexmock/test_unit_integration'
 
 if defined?(MiniTest)
-  module MiniTest
-    class Unit
-      class TestCase
-        include FlexMock::ArgumentTypes
-        include FlexMock::MockContainer
-
-        # Alias the original teardown behavior for later use.
-        alias :flexmock_original_teardown :teardown
-
-        # Teardown the test case, verifying any mocks that might have been
-        # defined in this test case.
-        def teardown
-          flexmock_teardown
-          flexmock_original_teardown
-        end
-      end
-    end
-  end
+  require 'flexmock/minitest_testcase_extensions'
 else
-  module Test
-    module Unit
-      class TestCase
-        include FlexMock::ArgumentTypes
-        include FlexMock::MockContainer
-
-        # Alias the original teardown behavior for later use.
-        alias :flexmock_original_teardown :teardown
-
-        # Teardown the test case, verifying any mocks that might have been
-        # defined in this test case.
-        def teardown
-          flexmock_teardown
-          flexmock_original_teardown
-        end
-
-      end
-    end
-  end
+  require 'flexmock/testunit_testcase_extensions'
 end
---
+++ lib/flexmock/testunit_testcase_extensions.rb
@@ -0,0 +1,18 @@
+module Test
+  module Unit
+    class TestCase
+      include FlexMock::ArgumentTypes
+      include FlexMock::MockContainer
+
+      # Alias the original teardown behavior for later use.
+      alias :flexmock_original_teardown :teardown
+
+      # Teardown the test case, verifying any mocks that might have been
+      # defined in this test case.
+      def teardown
+        flexmock_teardown
+        flexmock_original_teardown
+      end
+    end
+  end
+end
