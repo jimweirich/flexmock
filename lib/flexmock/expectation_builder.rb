e4133c55d10577765ab6ba660d39883c4a06d6fc
Jim Weirich <jim.weirich@gmail.com>
2013-05-09 10:08:25 -0400

    Move CompositeExpectation into its own file.
    And add copyright notice to expectation builder.

--- lib/flexmock/expectation_builder.rb
+++ lib/flexmock/expectation_builder.rb
@@ -1,3 +1,16 @@
+#!/usr/bin/env ruby
+
+#---
+# Copyright 2003-2013 by Jim Weirich (jim.weirich@gmail.com).
+# All rights reserved.
+
+# Permission is granted for use, copying, modification, distribution,
+# and distribution of modified versions of this work as long as the
+# above copyright notice is included.
+#+++
+
+require 'flexmock/composite_expectation'
+
 class FlexMock
 
   class ExpectationBuilder
